20d73aba6a3c890f7bbc74141b1d9d61d184f0b1
Alex Williams <alex.c.williams91@gmail.com>
2018-03-15 21:00:38 +0000

    Pass any auth headers into the axios

--- src/api/base.js
+++ src/api/base.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
+import auth from '../auth'
 
 export const http = axios.create({
-  baseURL: `http://api.eataway.co.uk/`
+  baseURL: `http://api.eataway.co.uk/`,
+  headers: auth.getAuthHeader()
 })
